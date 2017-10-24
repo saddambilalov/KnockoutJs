fadf669152e3aab9ab97a66f927d72c0715432a7
sadda <sadda@NAZHQ001SG778>
2017-10-24 20:00:02 +0200

    Add, edit operations implemented

--- AspNetMVCTest/Knockout/IndexViewModel.js
+++ AspNetMVCTest/Knockout/IndexViewModel.js
@@ -2,12 +2,16 @@
 function IndexViewModel() {
     this.firstName = ko.observable("");
     this.lastName = ko.observable("");
+    this.isAdd = ko.observable(true);
 
     // observable arrays are update binding elements upon array changes
     this.students = ko.observableArray([]);
 
     this.addStudent = () => {
+        this.isAdd(true);
+
         this.students.push({
+            id: Date.now(),
             firstName: this.firstName(),
             lastName: this.lastName()
         });
@@ -16,7 +20,29 @@ function IndexViewModel() {
         this.lastName("");
     };
 
+    this.editStudent = (student) => {
+        this.isAdd(false);
 
+        this.firstName(student.firstName);
+        this.lastName(student.lastName);
+    }
+
+    this.saveStudent = () => {
+        this.isAdd(true);
+
+        this.students.push({
+            id: Date.now(),
+            firstName: this.firstName(),
+            lastName: this.lastName()
+        });
+
+        this.firstName("");
+        this.lastName("");
+    };
+
+    this.removeStudent = (student) => {
+        this.students.remove(student);
+    }
 }
 
 // Activates knockout.js
