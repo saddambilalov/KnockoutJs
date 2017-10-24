5a5cdb7b22d8b7e385db7b4a98591e38460538f9
sadda <sadda@NAZHQ001SG778>
2017-10-24 23:03:27 +0200

    Custom Global filter implemented

--- AspNetMVCTest/App_Start/FilterConfig.cs
+++ AspNetMVCTest/App_Start/FilterConfig.cs
@@ -1,5 +1,5 @@
-﻿using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using AspNetMVCTest.Filters;
 
 namespace AspNetMVCTest
 {
@@ -8,6 +8,7 @@ namespace AspNetMVCTest
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new CustomFilterAttribute());
         }
     }
 }
---
+++ AspNetMVCTest/Filters/CustomFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace AspNetMVCTest.Filters
+{
+    public class CustomFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            //You may fetch data from database here 
+            filterContext.Controller.ViewBag.GreetMesssage = "Hello Foo";
+            base.OnResultExecuting(filterContext);
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var controllerName = filterContext.RouteData.Values["controller"];
+            var actionName = filterContext.RouteData.Values["action"];
+            var message = $"onactionexecuting controller:{controllerName} action:{actionName}";
+            Debug.WriteLine(message, "Action Filter Log");
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
