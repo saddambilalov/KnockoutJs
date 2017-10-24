2f34fd2d21a33abc24eacbc59344134e2841e91b
sadda <sadda@NAZHQ001SG778>
2017-10-24 22:36:41 +0200

    Cashing added to the web pages.

--- AspNetMVCTest/Controllers/HomeController.cs
+++ AspNetMVCTest/Controllers/HomeController.cs
@@ -1,13 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace AspNetMVCTest.Controllers
 {
     public class HomeController : Controller
     {
+        //[OutputCache(Duration = 30, VaryByParam = "none", Location = OutputCacheLocation.Server)]
         public ActionResult Index()
         {
-            //throw new Exception("An erro occured when progress the request");
+            if (System.Web.HttpContext.Current.Cache["count"] == null)
+            {
+                var list = new List<string>();
+
+                for (var i = 0; i < 5000000; i++)
+                {
+                    list.Add(i + "_new_" + i);
+                }
+
+                var count = list.Count(str => str.StartsWith("1"));
+
+                System.Web.HttpContext.Current.Cache["count"] = count;
+            }
+
+            ViewBag.Count = System.Web.HttpContext.Current.Cache["count"];
 
             return View();
         }
